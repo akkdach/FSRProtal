1013dafc1e699a47838e6cc244a8f0620f0766e5
unit7761-cpu <akkdach@gmail.com>
2026-03-27 08:06:57 +0700

    edit name api

--- onelake-middleware/src/routes/api.js
+++ onelake-middleware/src/routes/api.js
@@ -21,11 +21,11 @@ router.get('/dispatch-pending-new-customer', (req, res) => proIoTController.getD
 router.get('/dispatch-pending-cooler', (req, res) => proIoTController.getDispatchPendingCooler(req, res));
 router.get('/dispatch-pending', (req, res) => proIoTController.getDispatchPendingAll(req, res));
 
-// Project: FSR Protal
-// Using SQL Service for direct queries
+// Project: Other SQL Queries (Not FSR Protal)
 const fsrProtalControllerSql = require('../controllers/fsrProtalController_sql');
-router.get('/fsr-protal/bom-referbush', (req, res) => fsrProtalControllerSql.getBomReferbush(req, res));
+router.get('/bom-referbush', (req, res) => fsrProtalControllerSql.getBomReferbush(req, res));
 
+// Project: FSR Protal
 router.get('/fsr-protal/orders', (req, res) => fsrProtalController.getOrders(req, res));
 router.get('/service-header', (req, res) => fsrProtalController.getServiceHeader(req, res));
 
